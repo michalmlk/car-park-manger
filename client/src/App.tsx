92d6575cddba6a5afefe19949ace6a25cc24d122
michalmlk <michal.melka1@gmail.com>
2024-07-17 19:30:11 +0200

    ui changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,11 +4,17 @@ import { SignedIn, SignedOut, SignInButton } from '@clerk/clerk-react';
 import { Outlet } from 'react-router-dom';
 import { AppWrapper } from './App.styles.tsx';
 import TopBar from './components/topbar/TopBar.tsx';
+import { DirectionsCar } from '@mui/icons-material';
+import { LoginPageHeader } from './components/ui/LoginPageHeader.styles.tsx';
 
 export default function App() {
   return (
     <>
       <SignedOut>
+        <LoginPageHeader>
+          <h2>Car park manager</h2>
+          <DirectionsCar fontSize="large" />
+        </LoginPageHeader>
         <SignInButton />
       </SignedOut>
       <SignedIn>
---
+++ client/src/components/ui/LoginPageHeader.styles.tsx
@@ -0,0 +1,13 @@
+import { styled } from 'styled-components';
+
+export const LoginPageHeader = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+
+  h2 {
+    font-size: 2rem;
+    font-weight: lighter;
+  }
+`;
